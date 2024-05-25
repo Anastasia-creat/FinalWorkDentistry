9452958d8f8c05811a0c31c8a0d53f831981e0db
Anastasia <kochkina0226@mail.ru>
2024-05-25 22:11:47 +0500

    ReadyDent

---
+++ FinalWorkDentistry/Data/Migrations/20240525170706_First.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalWorkDentistry.Data.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "CategoryDoctor",
+                columns: table => new
+                {
+                    CategoryDoctorId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryDoctor", x => x.CategoryDoctorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryService",
+                columns: table => new
+                {
+                    CategoryServiceId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryService", x => x.CategoryServiceId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Requests",
+                columns: table => new
+                {
+                    RequestId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Symptoms = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Time = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Requests", x => x.RequestId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    DoctorId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoctorCategoryCategoryDoctorId = table.Column<long>(type: "bigint", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Job = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.DoctorId);
+                    table.ForeignKey(
+                        name: "FK_Doctors_CategoryDoctor_DoctorCategoryCategoryDoctorId",
+                        column: x => x.DoctorCategoryCategoryDoctorId,
+                        principalTable: "CategoryDoctor",
+                        principalColumn: "CategoryDoctorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NameReview = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryDoctorId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_Reviews_CategoryDoctor_CategoryDoctorId",
+                        column: x => x.CategoryDoctorId,
+                        principalTable: "CategoryDoctor",
+                        principalColumn: "CategoryDoctorId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    ServiceId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ServiceCategoryCategoryServiceId = table.Column<long>(type: "bigint", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.ServiceId);
+                    table.ForeignKey(
+                        name: "FK_Services_CategoryService_ServiceCategoryCategoryServiceId",
+                        column: x => x.ServiceCategoryCategoryServiceId,
+                        principalTable: "CategoryService",
+                        principalColumn: "CategoryServiceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_DoctorCategoryCategoryDoctorId",
+                table: "Doctors",
+                column: "DoctorCategoryCategoryDoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_CategoryDoctorId",
+                table: "Reviews",
+                column: "CategoryDoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_ServiceCategoryCategoryServiceId",
+                table: "Services",
+                column: "ServiceCategoryCategoryServiceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Requests");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "CategoryDoctor");
+
+            migrationBuilder.DropTable(
+                name: "CategoryService");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ FinalWorkDentistry/Startup.cs
@@ -0,0 +1,199 @@
+
+using FinalWorkDentistry.Abstract;
+using FinalWorkDentistry.AutoMapping;
+using FinalWorkDentistry.BlazorServices;
+using FinalWorkDentistry.DataAccessLayer;
+using FinalWorkDentistry.Domains;
+using FinalWorkDentistry.Models;
+
+using FinalWorkDentistry.UsersRoles;
+using KursovayaBlazorNet6.ServerHub;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Smart.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+
+namespace FinalWorkDentistry
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.HttpOnly = false;
+                options.Cookie.IsEssential = false; //---------
+            });
+
+            //  services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
+            services.AddScoped<IAuthorizationHandler, PermissionAuthorizationHandler>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services
+                .AddDefaultIdentity<ApplicationUser>(o => {
+                    o.Password.RequiredLength = 3;
+                    o.Password.RequireNonAlphanumeric = false;
+                    o.Password.RequiredUniqueChars = 0;
+                    o.Password.RequireDigit = false;
+                    o.Password.RequireLowercase = false;
+                    o.Password.RequireUppercase = false;
+                })
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddMvc();
+
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            services.AddServerSideBlazor();
+            // services.AddSingleton<WeatherForecastService>();
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
+            });
+
+                services.AddTransient<IRepository<Service>, ServiceSqlRepository>();
+
+         //   services.AddTransient<DoctorModel>();
+         //services.AddTransient<CategoryDoctor>();
+
+         //   services.AddTransient<DoctorBriefModel>();
+
+            services.AddTransient<IRepository<Doctor>, DoctorSqlRepository>();
+                services.AddTransient<IRepository<Reviews>, ReviewsSqlRepository>();
+            services.AddTransient<IRepository<Request>, RequestSqlRepository>();
+            services.AddTransient<IRepository<CategoryService>, CategoryUslugiSqlRepository>();
+                services.AddTransient<IRepository<CategoryDoctor>, CategoryMedicSqlRepository>();
+
+                // добавили сервис SignalR
+                services.AddSignalR();
+
+            services.AddTransient<BlazorService>();
+            services.AddTransient<BlazorDoctors>();
+           
+
+            services.AddSmart();
+
+            services.AddHttpContextAccessor();
+                services.AddScoped<HttpContextAccessor>();
+               // services.AddTransient<BlazorCart>();
+            }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
+
+
+ 
+
+public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseDatabaseErrorPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+                app.UseSession();
+            app.Use(async delegate (HttpContext Context, Func<Task> Next)
+            {
+                //this throwaway session variable will "prime" the SetString() method
+                //to allow it to be called after the response has started
+                var TempKey = Guid.NewGuid().ToString(); //create a random key
+                Context.Session.Set(TempKey, System.Array.Empty<byte>()); //set the throwaway session variable
+                Context.Session.Remove(TempKey); //remove the throwaway session variable
+                await Next(); //continue on with the request
+            });
+            app.UseRouting();
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    /*
+                    endpoints.MapControllerRoute(
+                        name: "navCategories",
+                        pattern: "{categoryName}/Page{page}",
+                        defaults: new { Controller = "Product", action = "ListView" });
+
+                    endpoints.MapControllerRoute(
+                        name: "navCategories",
+                        pattern: "{categoryName}",
+                        defaults: new { Controller = "Product", action = "ListView", page = 1 });
+
+                    endpoints.MapControllerRoute(
+                        name: "navigationPage",
+                        pattern: "Products/Page{page}", // Products/Page10
+                        defaults: new { Controller = "Product", action = "ListView" });
+                    */
+
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Blazor}/{action=IndexMain}/{id?}");
+
+                    endpoints.MapRazorPages();
+                });
+
+                ModelsMapping.InitializeMappingService();
+                ModelsMapping.InitializeMappingDoctor();
+
+                using (var scope = app.ApplicationServices.CreateScope())
+                {
+                    DataSeeder.SeedServices(scope.ServiceProvider);
+                    DataSeeder.SeedRoles(roleManager);
+                    DataSeeder.SeedUsers(userManager);
+                }
+
+            // добавляем маршрут для поодключения к чату!
+            app.UseEndpoints(endpoints =>
+
+
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapHub<ChatHub>("/chathub");
+                // endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+        }
+    } 
+
