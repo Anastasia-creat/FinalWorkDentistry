a6212f4c5f477ee908ebfa90f010c300a305bea8
Anastasia <kochkina0226@mail.ru>
2024-06-18 02:20:03 +0500

    Authorization

---
+++ FinalWorkDentistry/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FinalWorkDentistry.DataAccessLayer;
+using FinalWorkDentistry.UsersRoles;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FinalWorkDentistry.Areas.Identity.IdentityHostingStartup))]
+namespace FinalWorkDentistry.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
